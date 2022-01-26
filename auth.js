045e18654e8d2a1b7b2ecc37a223e8c8ada739b3
Mikalai-Hurinovich <gurinovich-ns@mail.ru>
2022-01-26 16:33:43 +0300

    features/jwt-token

--- auth.js
+++ auth.js
@@ -1,4 +1,6 @@
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const users = require('./database/users');
 
 exports.authenticate = function (req, res, next) {
     req.body.username = req.body.username.toLowerCase();
@@ -9,19 +11,59 @@ exports.authenticate = function (req, res, next) {
         if (!user) {
             res.sendStatus(403);
         }
+
+        const token = jwt.sign({
+            _id: user.id,
+        }, 'secret', {
+            expiresIn: "1d"
+        })
+
+        res.cookie('jwt', token, {
+            httpOnly: true,
+        })
+
         req.logIn(user, function (err) {
             if (err) {
                 return next(err);
             }
-            res.send({success: true, user: user});
+
+            res.send({success: true, user, token});
         })
     })
     auth(req, res, next);
 };
 
 exports.getCurrentIdentity = function (req, res) {
-    res.status(200).send(req.user);
+    const cookie = req.cookies['jwt'];
+
+    jwt.verify(cookie, 'secret', (err, decoded) => {
+        if (err) {
+
+            res.send(err);
+            res.end();
+        } else {
+            const user = users.find(u => u.id === decoded._id);
+            res.status(200).send(user);
+            res.end();
+        }
+    });
+}
+
+exports.logOut = function (req, res) {
+    req.logout();
     res.end();
+}
+
+exports.isTokenActive = function (req, res) {
+    jwt.verify(req.body.token, 'secret', (err) => {
+        if (err) {
+            res.send({error: err.name, isActive: false})
+            res.end();
+        } else {
+            res.send({isActive: true})
+            res.end();
+        }
+    })
 }
 
 exports.requiresApiLogin = function (req, res, next) {
